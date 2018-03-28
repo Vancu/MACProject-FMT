af27f092e110ddbe71110384374fed85be0d14a4
Andrew.Yeh <ayeh6@ucmerced.edu>
2018-03-28 15:49:45 -0700

    bus line screen works and is functional

--- app/src/main/java/com/vancu/findmytrackalpha1/SearchStops.java
+++ app/src/main/java/com/vancu/findmytrackalpha1/SearchStops.java
@@ -26,27 +26,34 @@ public class SearchStops extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_stops);
+
         BusCompany = (Spinner) findViewById(R.id.busCompanyspinner);
         adapter = ArrayAdapter.createFromResource(this,R.array.buseCompanyarray,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         BusCompany.setAdapter(adapter);
+
         BusID = (Spinner) findViewById(R.id.BusIDspinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.emptySpinner,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         BusID.setAdapter(adapter);
+
         Ranges = (Spinner) findViewById(R.id.Ranges);
         adapter = ArrayAdapter.createFromResource(this,R.array.MileRanges,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Ranges.setAdapter(adapter);
+
         BusCompany.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
             {
                 Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
-                if(position == 1)
+                if(position == 0)
                 {
                     //sets BusID to cattracks
                     adapter = ArrayAdapter.createFromResource(SearchStops.this,R.array.CattracksIDarray,android.R.layout.simple_spinner_item);
                     BusID.setAdapter(adapter);
                 }
-                else if(position == 2)
+                else if(position == 1)
                 {
                     adapter = ArrayAdapter.createFromResource(SearchStops.this,R.array.MercedTheBusIDarray,android.R.layout.simple_spinner_item);
                     BusID.setAdapter(adapter);
@@ -63,7 +70,7 @@ public class SearchStops extends AppCompatActivity
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
             {
-                //Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent)
@@ -76,7 +83,7 @@ public class SearchStops extends AppCompatActivity
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
             {
-                //Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent)
