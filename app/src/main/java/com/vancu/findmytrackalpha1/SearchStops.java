1b791728daf2b3354a1e385d212fc52f9647e537
Andrew.Yeh <ayeh6@ucmerced.edu>
2018-03-28 15:22:52 -0700

    changes to bus line screen, crashes again

--- app/src/main/java/com/vancu/findmytrackalpha1/SearchStops.java
+++ app/src/main/java/com/vancu/findmytrackalpha1/SearchStops.java
@@ -22,46 +22,35 @@ public class SearchStops extends AppCompatActivity
     ArrayAdapter<CharSequence> adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_stops);
         BusCompany = (Spinner) findViewById(R.id.busCompanyspinner);
         adapter = ArrayAdapter.createFromResource(this,R.array.buseCompanyarray,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         BusCompany.setAdapter(adapter);
-        BusID = (Spinner) findViewById(R.id.CattrackIDspinner);
-        Ranges = (Spinner) findViewById(R.id.Ranges);
-        Ranges.setAdapter(adapter);
+        BusID = (Spinner) findViewById(R.id.BusIDspinner);
         BusID.setAdapter(adapter);
-        Ranges.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
-            {
-                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent)
-            {
-
-            }
-        });
-        BusID.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
-            {
-                Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent)
-            {
-
-            }
-        });
+        Ranges = (Spinner) findViewById(R.id.Ranges);
+        adapter = ArrayAdapter.createFromResource(this,R.array.MileRanges,android.R.layout.simple_spinner_item);
+        Ranges.setAdapter(adapter);
         BusCompany.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
             {
                 Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+                if(position == 1)
+                {
+                    //sets BusID to cattracks
+                    adapter = ArrayAdapter.createFromResource(SearchStops.this,R.array.CattracksIDarray,android.R.layout.simple_spinner_item);
+                    BusID.setAdapter(adapter);
+                }
+                else if(position == 2)
+                {
+                    adapter = ArrayAdapter.createFromResource(SearchStops.this,R.array.MercedTheBusIDarray,android.R.layout.simple_spinner_item);
+                    BusID.setAdapter(adapter);
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent)
@@ -69,9 +58,32 @@ public class SearchStops extends AppCompatActivity
 
             }
         });
+        BusID.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
+            {
+                //Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
 
+            }
+        });
+        Ranges.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,int position,long id)
+            {
+                //Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+" is selected",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
 
-
+            }
+        });
     }
 
 }
