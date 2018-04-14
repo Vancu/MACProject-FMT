5ce4869d9caa248dcfdc842188520b1a659b9580
Vancu <super_paper_mario906@yahoo.com>
2018-04-13 19:10:12 -0700

    Map now shows user's location. database still needs encryption

--- app/src/main/java/com/vancu/findmytrackalpha1/LoggedInViewMapActivity.java
+++ app/src/main/java/com/vancu/findmytrackalpha1/LoggedInViewMapActivity.java
@@ -3,11 +3,14 @@ package com.vancu.findmytrackalpha1;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -20,13 +23,31 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.annotations.Icon;
 import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngineListener;
+import com.mapbox.services.android.telemetry.location.LocationEnginePriority;
+import com.mapbox.services.android.telemetry.location.LocationEngineProvider;
+import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
+import com.mapbox.services.android.telemetry.permissions.PermissionsListener;
 import com.mapbox.services.commons.geojson.Feature;
 import com.mapbox.services.commons.geojson.FeatureCollection;
 import com.mapbox.services.commons.geojson.Point;
 import com.vancu.findmytrackalpha1.utils.BottomNavigationViewHelper;
+
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngineListener;
+import com.mapbox.services.android.telemetry.location.LocationEnginePriority;
+import com.mapbox.services.android.telemetry.permissions.PermissionsListener;
+import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +62,12 @@ public class LoggedInViewMapActivity extends AppCompatActivity implements OnMapR
     private MapboxMap mapboxMap;
     private MapView mapView;
 
+    private PermissionsManager permissionsManager;
+    private PermissionsListener permissionsListener;
+    private LocationLayerPlugin locationPlugin;
+    private LocationEngine locationEngine;
+    private LocationEngineListener locationEngineListener;
+    private Location originLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +83,78 @@ public class LoggedInViewMapActivity extends AppCompatActivity implements OnMapR
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                LoggedInViewMapActivity.this.mapboxMap = mapboxMap;
+                enableLocationPlugin();
+            }
+        });
 
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationPlugin() {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Create a location engine instance
+            initializeLocationEngine();
+
+            locationPlugin = new LocationLayerPlugin(mapView, mapboxMap, locationEngine);
+            locationPlugin.setLocationLayerEnabled(LocationLayerMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(permissionsListener);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void initializeLocationEngine() {
+        locationEngine = new LocationEngineProvider(this).obtainBestLocationEngineAvailable();
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null) {
+            setCameraPosition(lastLocation);
+        } else {
+            locationEngine.addLocationEngineListener(locationEngineListener);
+        }
+    }
+
+    private void setCameraPosition(Location location) {
+        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(location.getLatitude(), location.getLongitude()), 16));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    //@Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enableLocationPlugin();
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    //@Override
+    @SuppressWarnings( {"MissingPermission"})
+    public void onConnected() {
+        locationEngine.requestLocationUpdates();
+    }
+
+    //@Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            setCameraPosition(location);
+            locationEngine.removeLocationEngineListener(locationEngineListener);
+        }
     }
 
     /*
@@ -147,8 +245,16 @@ public class LoggedInViewMapActivity extends AppCompatActivity implements OnMapR
 
 
     @Override
-    public void onStart() {
+    @SuppressWarnings( {"MissingPermission"})
+    protected void onStart() {
         super.onStart();
+        if (locationEngine != null) {
+            locationEngine.requestLocationUpdates();
+        }
+
+        if (locationPlugin != null) {
+            locationPlugin.onStart();
+        }
         mapView.onStart();
     }
 
@@ -167,6 +273,12 @@ public class LoggedInViewMapActivity extends AppCompatActivity implements OnMapR
     @Override
     public void onStop() {
         super.onStop();
+        if (locationEngine != null) {
+            locationEngine.removeLocationUpdates();
+        }
+        if (locationPlugin != null) {
+            locationPlugin.onStop();
+        }
         mapView.onStop();
     }
 
@@ -180,6 +292,9 @@ public class LoggedInViewMapActivity extends AppCompatActivity implements OnMapR
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+        if (locationEngine != null) {
+            locationEngine.deactivate();
+        }
     }
 
     @Override
