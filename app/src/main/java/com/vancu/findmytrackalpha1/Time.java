65fa33660f985559a5d15ccf1174ddc240eccbf7
Andrew.Yeh <ayeh6@ucmerced.edu>
2018-04-14 21:43:03 -0700

    Search Stops now implements moving to different activity using putExtra, more strings in the spinners

---
+++ app/src/main/java/com/vancu/findmytrackalpha1/Time.java
@@ -0,0 +1,38 @@
+package com.vancu.findmytrackalpha1;
+
+public class Time
+{
+    int hour;
+    int minute;
+
+    public Time() {
+        hour = 0;
+        minute = 0;
+    }
+
+    public Time(int hour, int minute) {
+        if(hour >= 0 && hour <= 24) {
+            this.hour = hour;
+        }
+        if(minute >=0 && minute <= 59) {
+            this.minute = minute;
+        }
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+}
